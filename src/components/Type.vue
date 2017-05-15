8073b5741b5f315e647aa84cfdbd52dfe356a2dd
infernalslam <infernal-slam@hotmail.com>
2017-05-16 06:44:18 +0700

    refar

--- src/components/Type.vue
+++ src/components/Type.vue
@@ -7,16 +7,37 @@
       <div class="container">
         <div class="columns">
 
-
+          <div class="column"></div>
 
           <div class="column">
 
-            <div class="card"></div>
+            <div class="card">
+              <img src="http://www.theinternship.io/_nuxt/img/designer.5ee021f.jpg">
+              <div class="card-detail">
+                <h1 class="title"> <b>ฝึกงาน</b>  </h1>
+                <b>ฝึกงาน 2 เดือน  1 มิ.ย. - 31 ก.ค. 25560</b>
+                <br>
+                <a class="button is-danger"><b>ลงทะเบียน</b></a>
+              </div>
+            </div>
 
 
           </div>
 
 
+
+
+          <div class="column">
+            <div class="card">
+              <img src="http://www.theinternship.io/_nuxt/img/dev.be1249b.jpg">
+              <div class="card-detail">
+                <h1 class="title"><b>สหกิจศึกษา</b></h1>
+                <b>ฝึกงาน 6 เดือน  1 มิ.ย. - 31 พ.ย. 25560</b>
+                <br>
+                <a class="button is-danger"><b>ลงทะเบียน</b></a>
+              </div>
+            </div>
+          </div>
 
 
           <div class="column"></div>
@@ -43,6 +64,28 @@ export default {
 .is-bg {
   background: linear-gradient(135deg,#1a8efa,#5931b5);
   /*background: linear-gradient(to right, rgba(255,82,122,1) 0%, rgba(231,67,64,1) 100%);*/
+}
+
+.card {
+  clear: both;
+  height: 200px;
+  display: block;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0 2px 3px hsla(0,0%,4%,.1), 0 0 0 1px hsla(0,0%,4%,.1);
+  color: #4a4a4a;
+  max-width: 100%;
+  position: relative;
+}
+.card img {
+    height: 160px;
+    float: left;
+}
+
+.card .card-detail {
+    margin-left: 90px;
 }
 
 </style>
