9d9b40dcc36807e25fa826a1fe76aae621970464
infernalslam <infernal-slam@hotmail.com>
2017-05-20 07:29:28 +0700

    add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
       <!-- <div class="arrow-right">
         <span> หน้า Sign in ของอาจารย์ค่ะ </span>
       </div> -->
-      <div class="columns">
+      <!-- <div class="columns">
         <div class="column is-5"></div>
         <div class="column is-5"></div>
         <div class="column is-2">
@@ -15,6 +15,22 @@
             <img :src="photoURL" width="50px" height="50%">
           </div>
         </div>
+      </div> -->
+
+
+      <div class="hero-head">
+        <header class="nav">
+          <div class="container">
+            <div class="nav-right nav-menu">
+              <span class="nav-item">
+                <a class="button is-danger is-focused" v-if="adminState === false" style="color: #fff;" @click="logingFacebook()"> <b>หน้า Sign in ของอาจารย์ค่ะ</b></a>
+                <a class="button is-success is-focused" v-if="adminState === true" style="color: #fff; cursor: pointer;" @click="logOut()">
+                  <img :src="photoURL">&nbsp&nbsp <b> logOut | {{ displayName }} กดเพื่อออก</b>
+                </a>
+              </span>
+            </div>
+          </div>
+        </header>
       </div>
 
 
@@ -61,6 +77,10 @@ export default {
 
 <style scoped>
 
+
+img {
+  border-radius: 66px;
+}
 .bg  {
     background: linear-gradient(135deg,#1a8efa,#5931b5);
     /*background: linear-gradient(to right, rgba(255,82,122,1) 0%, rgba(231,67,64,1) 100%);*/
@@ -130,43 +150,5 @@ export default {
     70% { top: -0.3em; }
     100% { top: 0; }
 }
-
-/*.arrow-up {
-  width: 0;
-  height: 0;
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-
-  border-bottom: 5px solid black;
-}
-
-.arrow-down {
-  width: 0;
-  height: 0;
-  border-left: 20px solid transparent;
-  border-right: 20px solid transparent;
-
-  border-top: 20px solid #f00;
-}*/
-
-/*.arrow-right {
-      position: relative;
-      top: 0;
-      left: 0;
-      width: 0;
-      height: 0;
-      border-bottom: 111px solid transparent;
-      border-left: 114px solid #fefefe;
-}*/
-
-/*.arrow-left {
-  width: 0;
-  height: 0;
-  border-top: 10px solid transparent;
-  border-bottom: 10px solid transparent;
-
-  border-right:10px solid blue;
-}*/
-
 
 </style>
